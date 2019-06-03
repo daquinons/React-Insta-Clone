ba7aca2ae451b783ee20190cffbd52cc7a264155
David Quiñones <daquinons@users.noreply.github.com>
2019-06-03 15:43:02 +0200

    Fix srcSet attribute

--- instagram-app/src/components/SearchBar/SearchBar.jsx
+++ instagram-app/src/components/SearchBar/SearchBar.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { Container, Row, Col } from 'reactstrap';
+import { Row, Col } from 'reactstrap';
 
 const SearchBar = () => {
   return(
       <Row className="text-center">
         <Col className="justify-content-center align-self-center" xs="6" sm="4">
-          <img className="img-fluid" src="" alt="" srcset="./img/ig_logo.png"/>
+          <img className="img-fluid" src="" alt="" srcSet="./img/ig_logo.png"/>
         </Col>
         <Col xs="6" sm="4" className="justify-content-center align-self-center">
           <input type="text" className="text-center" name="search" id="search-field" placeholder="Search" />
         </Col>
         <Col className="justify-content-center align-self-center" xs="6" sm="4">
-          <img className="img-fluid" src="" alt="" srcset="./img/ig_icons.png"/>
+          <img className="img-fluid" src="" alt="" srcSet="./img/ig_icons.png"/>
         </Col>
       </Row>
   )
