f83ccea100dbb151bab768caa249e24a16a88646
David Quiñones <daquinons@users.noreply.github.com>
2019-06-03 17:59:11 +0200

    Adjust style

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,23 +1,30 @@
-import React from 'react';
-import './App.css';
-import SearchBar from './components/SearchBar/SearchBar';
-import PostContainer from './components/PostContainer/PostContainer';
-import dummyData from './dummy-data';
+import React from "react";
+import "./App.css";
+import SearchBar from "./components/SearchBar/SearchBar";
+import PostContainer from "./components/PostContainer/PostContainer";
+import { Container, Row, Col } from "reactstrap";
+import dummyData from "./dummy-data";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <SearchBar />
-      </header>
-      <div id="App-main">
-        {dummyData.map((post, index) => {
-          return (
-            <div className="post-container" key={index}>
-              <PostContainer post={post} />
-            </div>
-        )})}
-      </div>
+      <Container fluid id="App-main">
+        <header className="App-header">
+          <SearchBar />
+        </header>
+      
+        <Row>
+          <Col sm="12" md={{ size: 6, offset: 3 }}>
+            {dummyData.map((post, index) => {
+              return (
+                <div className="post-container" key={index}>
+                  <PostContainer post={post} />
+                </div>
+              );
+            })}
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
--- instagram-app/src/components/SearchBar/SearchBar.jsx
+++ instagram-app/src/components/SearchBar/SearchBar.jsx
@@ -1,20 +1,34 @@
-import React from 'react';
-import { Row, Col } from 'reactstrap';
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import "./SearchBar.css";
 
 const SearchBar = () => {
-  return(
-      <Row className="text-center">
-        <Col className="justify-content-center align-self-center" xs="6" sm="4">
-          <img className="img-fluid" src="" alt="" srcSet="./img/ig_logo.png"/>
-        </Col>
-        <Col xs="6" sm="4" className="justify-content-center align-self-center">
-          <input type="text" className="text-center" name="search" id="search-field" placeholder="Search" />
-        </Col>
-        <Col className="justify-content-center align-self-center" xs="6" sm="4">
-          <img className="img-fluid" src="" alt="" srcSet="./img/ig_icons.png"/>
-        </Col>
-      </Row>
-  )
-}
+  return (
+    <Row className="text-center" id="header">
+      <Col className="justify-content-center align-self-center" xs="6" sm="4">
+        <img
+          className="img-fluid logo"
+          src=""
+          alt=""
+          srcSet="./img/ig_logo.png"
+        />
+      </Col>
+      <Col xs="6" sm="4" className="justify-content-center align-self-center">
+        <label>
+          <input
+            type="text"
+            className="text-center"
+            name="search"
+            id="search-field"
+            placeholder="Search"
+          />
+        </label>
+      </Col>
+      <Col className="justify-content-center align-self-center" xs="6" sm="4">
+        <img className="icons" src="" alt="" srcSet="./img/ig_icons.png" />
+      </Col>
+    </Row>
+  );
+};
 
 export default SearchBar;
