f086aeae1e62dc6d386b757e87c2fef6ccd6eaa9
David Quiñones <daquinons@users.noreply.github.com>
2019-06-03 14:27:29 +0200

    Add SearchBar

--- instagram-app/src/components/SearchBar/SearchBar.jsx
+++ instagram-app/src/components/SearchBar/SearchBar.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
 
 const SearchBar = () => {
-  return <div></div>;
+  return(
+      <Row className="text-center">
+        <Col className="justify-content-center align-self-center" xs="6" sm="4">
+          <img className="img-fluid" src="" alt="" srcset="./img/ig_logo.png"/>
+        </Col>
+        <Col xs="6" sm="4" className="justify-content-center align-self-center">
+          <input type="text" className="text-center" name="search" id="search-field" placeholder="Search" />
+        </Col>
+        <Col className="justify-content-center align-self-center" xs="6" sm="4">
+          <img className="img-fluid" src="" alt="" srcset="./img/ig_icons.png"/>
+        </Col>
+      </Row>
+  )
 }
 
 export default SearchBar;
