86654fbc72927a8f5f8776deaedcd3e763a93851
David Quiñones <daquinons@users.noreply.github.com>
2019-06-04 14:36:47 +0200

    Save comments in state of CommentSection

--- instagram-app/src/components/CommentSection/CommentSection.jsx
+++ instagram-app/src/components/CommentSection/CommentSection.jsx
@@ -1,32 +1,39 @@
-import React from 'react';
-import Comment from './Comment';
-import { Input } from 'reactstrap';
-import './CommentSection.css';
+import React, { useState, useEffect } from "react";
+import Comment from "./Comment";
+import { Input } from "reactstrap";
+import "./CommentSection.css";
 
 let CommentSection = ({ comments, timeAgo }) => {
+
+  const [storedComments, setStoredComments] = useState([]);
+
+  useEffect(() => {
+    setStoredComments(comments);
+  },[comments]);
+  
   return (
     <>
-    <div className="comment-section">
-      {comments.map((comment, index) => {
-        return(
-          <div key={index}>
-            <Comment comment={comment} />
-          </div>
-        );
-      })}
-    </div>
-    <div>
-      {timeAgo}
-      <hr />
-      <Input
-        type="text"
-        name="comment"
-        className="comment-form"
-        placeholder="Add a comment..."
-      />
-    </div>
+      <div className="comment-section">
+        {comments.map((comment, index) => {
+          return (
+            <div key={index}>
+              <Comment comment={comment} />
+            </div>
+          );
+        })}
+      </div>
+      <div>
+        {timeAgo}
+        <hr />
+        <Input
+          type="text"
+          name="comment"
+          className="comment-form"
+          placeholder="Add a comment..."
+        />
+      </div>
     </>
-  )
+  );
 };
 
 export default CommentSection;
