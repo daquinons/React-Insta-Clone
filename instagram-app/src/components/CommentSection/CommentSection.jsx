17321b1703c32179433287b06fd7443294ccc503
David Quiñones <daquinons@users.noreply.github.com>
2019-06-04 15:06:12 +0200

    Add new comments

--- instagram-app/src/components/CommentSection/CommentSection.jsx
+++ instagram-app/src/components/CommentSection/CommentSection.jsx
@@ -4,17 +4,40 @@ import { Input } from "reactstrap";
 import "./CommentSection.css";
 
 let CommentSection = ({ comments, timeAgo }) => {
-
-  const [storedComments, setStoredComments] = useState([]);
+  const [storedComments, setStoredComments] = useState(comments);
+  const [commentFormInput, setCommentFormInput] = useState("");
 
   useEffect(() => {
-    setStoredComments(comments);
-  },[comments]);
-  
+    setStoredComments(storedComments);
+  }, [storedComments]);
+
+  const addnewComment = () => {
+    const idLastCommentNumber = Number(
+      storedComments[storedComments.length - 1].id
+    );
+    let newComment = {
+      id: String(idLastCommentNumber + 1),
+      username: "arandomuser",
+      text: commentFormInput
+    };
+
+    setStoredComments(storedComments.concat(newComment));
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    addnewComment();
+    setCommentFormInput("");
+  };
+
+  const handleChange = event => {
+    setCommentFormInput(event.target.value);
+  };
+
   return (
     <>
       <div className="comment-section">
-        {comments.map((comment, index) => {
+        {storedComments.map((comment, index) => {
           return (
             <div key={index}>
               <Comment comment={comment} />
@@ -25,12 +48,16 @@ let CommentSection = ({ comments, timeAgo }) => {
       <div>
         {timeAgo}
         <hr />
-        <Input
-          type="text"
-          name="comment"
-          className="comment-form"
-          placeholder="Add a comment..."
-        />
+        <form onSubmit={handleSubmit.bind(this)}>
+          <Input
+            value={commentFormInput}
+            type="text"
+            name="comment"
+            className="comment-form"
+            placeholder="Add a comment..."
+            onChange={handleChange}
+          />
+        </form>
       </div>
     </>
   );
