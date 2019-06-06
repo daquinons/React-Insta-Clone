b8d103bd77ce106a8fdd9b75606cc2cb84c98112
David Quiñones <daquinons@users.noreply.github.com>
2019-06-06 14:30:27 +0200

    Refactor LoginPage to include styled-components

--- instagram-app/src/components/Login/LoginPage.jsx
+++ instagram-app/src/components/Login/LoginPage.jsx
@@ -1,10 +1,51 @@
 import React from "react";
 import { Button, Card, CardBody } from "reactstrap";
-import "./LoginPage.css";
+import styled from "styled-components";
+
+const StyledLoginPage = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 100px;
+
+  .form-login {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 10px 0;
+    width: 258px;
+    margin: 15px auto;
+  }
+
+  .card-login {
+    width: 348px;
+    border: 1px solid #e6e6e6 !important;
+    border-radius: 1px;
+  }
+
+  .form-input {
+    background: #fafafa;
+    border: 1px solid #efefef;
+    border-radius: 3px;
+    padding: 0 1rem;
+    margin-bottom: 15px;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+      Helvetica, Arial, sans-serif;
+    font-size: 14px;
+    color: #000;
+    width: 95%;
+  }
+
+  .login-button {
+    background-color: #3897f0;
+    font-weight: 600;
+    margin-top: 15px;
+    width: 95%;
+  }
+`;
 
 const LoginPage = ({ login }) => {
   return (
-    <div className="login-page text-center">
+    <StyledLoginPage className="text-center">
       <Card className="card-login">
         <CardBody>
           <img alt="" srcSet="./img/logo.svg" width="175px" />
@@ -29,7 +70,7 @@ const LoginPage = ({ login }) => {
           </div>
         </CardBody>
       </Card>
-    </div>
+    </StyledLoginPage>
   );
 };
 
