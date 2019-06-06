ab65f3d50078817a9f202e66c007aaf18c17fac0
David Quiñones <daquinons@users.noreply.github.com>
2019-06-06 13:45:22 +0200

    Add styled components to SearchBar

--- instagram-app/src/components/SearchBar/SearchBar.jsx
+++ instagram-app/src/components/SearchBar/SearchBar.jsx
@@ -1,6 +1,32 @@
 import React, { useState, useEffect } from "react";
 import { Row, Col } from "reactstrap";
-import "./SearchBar.css";
+import styled from "styled-components";
+
+const StyledHeader = styled.div`
+  background-color: white;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.0975);
+  padding-top: 0.7rem;
+  margin-bottom: 2rem;
+  height: 77px;
+
+  @media only screen and (max-width: 600px) {
+    height: 130px;
+  }
+  .logo {
+    max-height: 55px;
+  }
+
+  #search-field {
+    background-color: #fafafa;
+    border-radius: 3px;
+    border: solid 1px #dbdbdb;
+    margin-top: 0.75rem;
+  }
+
+  .icons {
+    height: 32px;
+  }
+`;
 
 const SearchBar = ({ onSearch, display }) => {
   const [shouldDisplay, setShouldDisplay] = useState(display);
@@ -14,9 +40,9 @@ const SearchBar = ({ onSearch, display }) => {
   }
 
   return (
-    <header className="App-header">
-      <Row className="text-center" id="header">
-        <Col className="justify-content-center align-self-center" >
+    <StyledHeader id="header">
+      <Row className="text-center">
+        <Col className="justify-content-center align-self-center">
           <img
             className="img-fluid logo"
             src=""
@@ -36,11 +62,11 @@ const SearchBar = ({ onSearch, display }) => {
             />
           </label>
         </Col>
-        <Col className="justify-content-center align-self-center" >
+        <Col className="justify-content-center align-self-center">
           <img className="icons" src="" alt="" srcSet="./img/ig_icons.png" />
         </Col>
       </Row>
-    </header>
+    </StyledHeader>
   );
 };
 
